b35df802c927be5abc4d7f8162255e9e223360bd
Ferdous Reza <ferdousreza631@gmail.com>
2025-05-20 22:22:05 +0600

    solutio9

---
+++ problem.py
@@ -0,0 +1,4 @@
+# String Concatenation: Write a Python program that takes two strings as input and concatenates them into a single string without using the `+` operator.
+
+concatenated_string = ''.join([input("Enter first string: "), input("Enter second string: ")])
+print("Concatenated String:", concatenated_string)
