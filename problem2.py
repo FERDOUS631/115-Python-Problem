bc43997b1b9060b22fb10daf079da0ccbc333b37
Ferdous Reza <ferdousreza631@gmail.com>
2025-05-20 18:48:49 +0600

    solution2

---
+++ problem2.py
@@ -0,0 +1,9 @@
+#  Even or Odd: Write a Python program that takes an integer as input and prints whether it is even or odd.
+
+a=int(input("Enter an integer: "))
+if a%2==0:
+    print(f"{a} is even")
+else:
+    print(f"{a} is odd")
+
+#an integer as input and prints whether it is even or odd
