5e8976109d41d6d29c44805ae9fb7763b50801b4
Ferdous Reza <ferdousreza631@gmail.com>
2025-05-20 18:37:32 +0600

    solution1

---
+++ problem1.py
@@ -0,0 +1,9 @@
+# Write a Python program to swap the values of two variables without using a temporary variable.
+
+int1 = 5
+int2 = 10
+# Swapping the values of int1 and int2
+print(f'Before swapping: {int1} {int2}')
+# Swapping the values
+print(f'After swapping: {int2} {int1}')
+# The values of int1 and int2 are swapped without using a temporary variable
