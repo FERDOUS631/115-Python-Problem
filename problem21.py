194a08d909e1ce0975bc95e0c54f6f7ff11e15d6
Ferdous Reza <ferdousreza631@gmail.com>
2025-05-21 10:20:49 +0600

    solution21

--- problem21.py
+++ problem21.py
@@ -5,4 +5,10 @@ i = 1
 while i <= number:
     factorial *= i
     i += 1
-print(f"The factorial of {number} is: {factorial}")
+    print(i)
+if number < 0:
+    print("Factorial is not defined for negative numbers.")
+elif number == 0:
+    print("The factorial of 0 is 1.")
+else:
+    print(f"The factorial of {number} is: {factorial}")
